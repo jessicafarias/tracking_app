6d978684a8693048712e45d5302115d8877e42c2
jessica.farias.rosado@gmail.com <jessica.farias.rosado@gmail.com>
2021-03-10 23:31:43 -0600

    docs: Creates toggle using react hooks
    docs: Create empty folders
    docs: Creates Toggle
    docs: Creates Header
    style: Adding color and images to Toggle component

---
+++ src/containers/Header.js
@@ -0,0 +1,13 @@
+import '../styles/header.css'
+
+const Header = () => {
+  return (
+    <div className="d-flex justify-content-center p-2 header-blue">
+      <p>
+        Track.it
+      </p>
+    </div>
+  );
+}
+
+export default Header;
